07365070749394be2b5364a274122bc6fe4b7a58
conoroshea1996 <conoshea12@gmail.com>
2019-10-04 20:01:11 +0100

    update styling and display image

--- src/components/Pokecard/Pokecard.js
+++ src/components/Pokecard/Pokecard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import '../Pokecard/Pokecard.css';
 
 function PokeCard({ pokemon }) {
     const { id, name, type, base_experience } = pokemon;
+    const baseImgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
     return (
-        <div>
+        <div className='pokecard'>
             <h2>{name}</h2>
             <h4>{type}</h4>
+            <img src={baseImgUrl + id.toString().padStart(3, '0') + '.png'} />
         </div>
     )
 }
